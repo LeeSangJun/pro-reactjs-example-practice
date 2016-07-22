d8ad92d9dbce44477ffecc1df8bc6d6215819bac
sangjun.lee <bluevia@hu-nam-nam-ui-iMac.local>
2016-07-22 14:58:22 +0900

    example02_card toggle 추가

--- source/Card.js
+++ source/Card.js
@@ -5,14 +5,31 @@ import React, { Component } from 'react'
 import CheckList from './CheckList';
 
 class Card extends Component{
+
+    constructor(){
+        super(...arguments);    //... Opnterator : 전개연산자 식이 여러 인수나 여러 요소 또는 여러 변수(비 구조화 할동용가 예성되는 곳에 확장될 수 있도록 합니다)
+        this.state = {
+            showDetails: false
+        }
+    }
+
     render() {
-        return (
-            <div className="card">
-                <div className="card__title">{this.props.title}</div>
+        let cardDetails;
+        if (this.state.showDetails){
+            cardDetails = (
                 <div className="card__details">
                     {this.props.description}
                     <CheckList cardId={this.props.id} tasks={this.props.tasks} />
                 </div>
+            )
+        }
+
+        return (
+            <div className="card">
+                <div className="card__title" onClick={
+                    ()=>this.setState({showDetails: !this.state.showDetails})
+                }>{this.props.title}</div>
+                { cardDetails }
             </div>
         )
     }
